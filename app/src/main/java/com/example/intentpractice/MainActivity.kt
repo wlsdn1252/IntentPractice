a00b15941847a023ff417e206116bfb995cadb52
강진우 <wlsdn1252@naver.com>
2022-12-30 11:54:51 +0900

    Intent 사용방법

--- app/src/main/java/com/example/intentpractice/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.intentpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+
+            // 다른 화면으로 이동하기
+            // Intent(출발지, 도착지)
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent) // 출발지, 도착지 정보가담긴 myIntent를 넣어준다.
+        }
     }
 }
