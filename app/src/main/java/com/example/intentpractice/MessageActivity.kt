535fa995c758e8d6e6175932250c64ac4bc2b255
강진우 <wlsdn1252@naver.com>
2022-12-30 15:49:26 +0900

    입력한 문구 들고 넘어가 뿌려주기 까지

--- app/src/main/java/com/example/intentpractice/MessageActivity.kt
+++ app/src/main/java/com/example/intentpractice/MessageActivity.kt
@@ -2,10 +2,17 @@ package com.example.intentpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_message.*
 
 class MessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
+
+        // 화면에 들어올때 첨부된 데이터를 덱스트뷰에 반영
+        // 메인 액티비티에서 String형태로 보낸 메세지를 받음 name은 동일해야 함
+        val intentMessage = intent.getStringExtra("message")
+
+        receivedMessageTxt.text = intentMessage
     }
 }
